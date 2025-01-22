a38965680381116b3a08fb1fd2f1cce7820113ca
sw_practice <133002803+junC1ion@users.noreply.github.com>
2025-01-22 21:32:18 +0900

    Update TestController.java

--- src/main/java/com/example/elice/TestController.java
+++ src/main/java/com/example/elice/TestController.java
@@ -15,7 +15,7 @@ public class TestController {
     @CrossOrigin(origins = "*", methods = {RequestMethod.POST, RequestMethod.GET, RequestMethod.OPTIONS})
     @GetMapping
     public String home() {
-    return "Hello Elice AWS Deploy TEST4444!!!!";
+    return "Hello Elice AWS Deploy TEST5555!!!!";
     }
 
     @CrossOrigin(origins = "*", methods = {RequestMethod.POST, RequestMethod.GET, RequestMethod.OPTIONS})
