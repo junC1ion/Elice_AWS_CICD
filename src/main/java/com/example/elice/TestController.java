12676e26ef5ebd29dc88d45d435c3470def3a071
Si WON <tldnjs4086@gmail.com>
2025-01-22 17:32:55 +0900

    #2

--- src/main/java/com/example/elice/TestController.java
+++ src/main/java/com/example/elice/TestController.java
@@ -15,7 +15,7 @@ public class TestController {
     @CrossOrigin(origins = "*", methods = {RequestMethod.POST, RequestMethod.GET, RequestMethod.OPTIONS})
     @GetMapping
     public String home() {
-    return "Hello Elice AWS Deploy TEST!!!!";
+    return "Hello Elice AWS Deploy TEST2222!!!!";
     }
 
     @CrossOrigin(origins = "*", methods = {RequestMethod.POST, RequestMethod.GET, RequestMethod.OPTIONS})
